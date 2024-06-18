1f2947b9e848f7e87c06b16d8d8dbea007184463
Vinayak Ghai <vghai36322@gmail.com>
2024-06-18 00:41:02 -0400

    running score + winner display completed

--- script.js
+++ script.js
@@ -25,7 +25,15 @@ const paper_btn = document.querySelector(".Paper");
 const scissors_btn = document.querySelector(".Scissors");
 
 const display_results = document.querySelector("#display_results");
-display_results.textContent = "Click any of the buttons to begin playing!"
+display_results.style.fontSize = "50px";
+display_results.style.color = "white";
+display_results.textContent = "Click any of the buttons to begin playing!";
+
+const running_us = document.querySelector(".user_score");
+running_us.style.fontSize = "50px";
+const running_cs = document.querySelector(".computer_score");
+running_cs.style.fontSize = "50px";
+
 
 rock_btn.addEventListener('click', () => {
     playRound("rock", getComputerChoice());
@@ -37,6 +45,23 @@ scissors_btn.addEventListener('click', () => {
     playRound("scissors", getComputerChoice());
 });
 
+function change_running_scores(user, computer) {
+    running_us.textContent = `User Score: ${user} `
+    running_cs.textContent = `Computer Score: ${computer} `
+}
+
+function display_winner () {
+    rock_btn.disabled = true;
+    paper_btn.disabled = true;
+    scissors_btn.disabled = true;
+    if (humanScore > computerScore){
+        running_us.textContent = `Final Score - User: ${humanScore} vs. Computer: ${computerScore}`;
+        running_cs.textContent = "Winner Winner, Chicken Dinner";
+    } else {
+        running_us.textContent = `Final Score - User: ${humanScore} vs. Computer: ${computerScore}`;
+        running_cs.textContent = "The Computer takes this one!";
+    }
+}
 
 function playRound(humanChoice, computerChoice){
     human = humanChoice.toLowerCase();
@@ -47,32 +72,62 @@ function playRound(humanChoice, computerChoice){
         if (computerChoice === "paper"){
             display_results.textContent = ("You lose! Paper beats Rock");
             computerScore++;
+            if (computerScore == 5){
+                display_winner();
+            } else {
+                change_running_scores(humanScore, computerScore);
+            }   
         } else {
             display_results.textContent = ("You Win! Rock beats Scissors");
             humanScore++;
+            if (humanScoreScore == 5){
+                display_winner();
+            } else {
+                change_running_scores(humanScore, computerScore);
+            }
         }
     }
     else if (human === "paper"){
         if (computerChoice === "rock"){
             display_results.textContent = ("You Win! Paper beats Rock");
             humanScore++;
+            if (humanScore == 5){
+                display_winner();
+            } else {
+                change_running_scores(humanScore, computerScore);
+            }   
         } else {
             display_results.textContent = ("You Lose! Scissors beats Paper");
             computerScore++;
+            if (computerScore == 5){
+                display_winner();
+            } else {
+                change_running_scores(humanScore, computerScore);
+            }   
         }
     }
     else if (human === "scissors"){
         if (computerChoice === "rock"){
             display_results.textContent = ("You lose! Rock beats Scissors");
             computerScore++;
+            if (computerScore == 5){
+                display_winner();
+            } else {
+                change_running_scores(humanScore, computerScore);
+            }   
         } else {
             display_results.textContent = ("You Win! Scissors beats Paper");
             humanScore++;
+            if (humanScore == 5){
+                display_winner();
+            } else {
+                change_running_scores(humanScore, computerScore);
+            }   
         }
     }
 }
 
-function playGame(){
+/*function playGame(){
     
     humanSelection = getHumanChoice();
     computerSelection = getComputerChoice();
@@ -87,6 +142,6 @@ function playGame(){
     } else {
         console.log("It was a tie game!")
     }
-}
+}*/
 
 
